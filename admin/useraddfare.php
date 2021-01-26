89061bb091612b050a38267685ddf3ad7fd35037
RavanSA <58227337+RavanSA@users.noreply.github.com>
2021-01-26 22:25:28 +0400

    Update useraddfare.php
    yearly payment changed to monthly payment

--- admin/useraddfare.php
+++ admin/useraddfare.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
  session_start();
 ?>
 <?php 
@@ -34,7 +34,7 @@ if(!isset($_SERVER['HTTP_REFERER'])){
   border-radius: 10px;
   box-sizing: border-box;
 }
-    input[type=password], select {
+    input[type=date], select {
   width: 80%;
   padding: 12px 20px;
   margin: 8px 0;
@@ -43,7 +43,6 @@ if(!isset($_SERVER['HTTP_REFERER'])){
   border-radius: 10px;
   box-sizing: border-box;
 }
-
 input[type=submit] {
   width: 80%;
   background-color: #4CAF50;
@@ -71,9 +70,12 @@ input[type=submit]:hover {
 }
 
 #logg {
+  width: 50%;
+  height: 50%;
   border-radius: 5px;
   background-color: white;
   padding: 20px;
+  text-align: center 
 
 }
 .blocktype-css {
@@ -101,15 +103,15 @@ include "../config/header.php";
  Apartment Manager</i></h1>
  <br>
 <h3> Create the monthly due for the user</h3>
- </div>
+ </div><!--
  <center><br>   
  <div class="container">
-  <div class="alert alert-info alert-dismissible" style="width: 65%">
+  <div class="alert alert-info alert-dismissible" style="width: 35%">
     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
     <strong>Read before taking any action!</strong> This section is a form created to pay the user's monthly fee.
   </div>
 </div>
-</center>
+</center>-->
  <?php
  
 $host = "localhost";
@@ -119,182 +121,77 @@ $db = "mydb";
   $dbconnection = new mysqli($host, $user, $pass,$db);
 
 
-	$nameErr = $emailErr = $genderErr = $passwordErr = $phonenumErr = $blcktypeErr = $aptnumbErr = " ";  
 
-    $email = "";
 	$userid = "";
-    $username = "";
-    $phonenumber = "";
-    $blocktype = "";
-    $apartmentnumber = "";
-    $totaldebt = "";
+    $monthlydue= "";
     $submitdate = "";
-    $feedescription ="";
-    $formValid = true;
-        $blccheck1 = "A";
-
+    $ispay= "";
+    $paiddate = "";
+    $feedescription= "";
 
     
-if(isset($_POST['userid'])){
-    $userid = $_POST['userid'];
+
+ if(isset($_POST['monthlydue'])){
+    $monthlydue=  $_POST['monthlydue'];
  } 
- if(isset($_POST['username'])){
-    $username = $_POST['username'];
- } 
- if(isset($_POST['email'])){
-    $email = $_POST['email'];
- }
- if(isset($_POST['blocktype'])){
-    $blocktype = $_POST['blocktype'];
- } 
- if(isset($_POST['phonenumber'])){
-    $phonenumber = $_POST['phonenumber'];
- } 
-  if(isset($_POST['apartmentnumber'])){
-    $apartmentnumber = $_POST['apartmentnumber'];
- } 
-  if(isset($_POST['totaldebt'])){
-    $totaldebt = $_POST['totaldebt'];
- }
-  if(isset($_POST['submitdate'])){
+
+ if(isset($_POST['submitdate'])){
     $submitdate = $_POST['submitdate'];
- }
-  if(isset($_POST['feedescription'])){
+ }  
+ if(isset($_POST['feedescription'])){
     $feedescription = $_POST['feedescription'];
- }
+ } 
+
     
  if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if(isset($_POST["submit"])){
-
-     if (empty($username)) {
-    $nameErr = "Name is required";
-  } else {
-    $username = test_input($_POST["username"]);
-    if (!preg_match("/^[a-zA-Z-' ]*$/", $username)) {
-      $nameErr = "Only letters and <br> white space allowed";
-      $formValid = false;
-    }
-  }
-
-     if (empty($phonenumber)) {
-    $phonenumErr = "Phone Number is required";
-  } else {
-    $phonenumber = test_input($_POST["phonenumber"]);
-   if (!preg_match("/\+?([0-9]{2})-?([0-9]{3})-?([0-9]{6,7})/ ", $phonenumber)) {
-      $phonenumErr = "Invalid Phone Number!!!";
-      $formValid = false;
-    }
-  }
-
-    if (empty($email)) {
-    $emailErr = "Email is required";
-  } else {
-    $email = test_input($_POST["email"]);
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email  format";
-      $formValid = false;
-    }
-  }
-
-
-
-    if (empty($blocktype)) {
-    $blcktypeErr = "Block number is required";
-  } else {
-    $blocktype = test_input($_POST["blocktype"]);
-   
-    if (substr_compare($blocktype,$blccheck1,0) != 0) {
-      $blcktypeErr = "Please write A (Only block A available)";
-      $formValid = false;
-    }
-  }
-
-    if (empty($apartmentnumber)) {
-    $aptnumbErr = "Apartment number is required";
-  } else {
-  
-    $apartmentnumber = $_POST["apartmentnumber"];
-    
-    if ($apartmentnumber < 0 || $apartmentnumber > 51) {
-      $aptnumbErr = "Available Apartment between 0 and 50";
-      $formValid = false;
-    }
-  }
-
- 
-$sql1 = "INSERT INTO feestransaction(userid, username, email, blocktype, phonenumber,
-apartmentnumber, totaldebt, submitdate, feedescription) VALUES ('".$userid."', '".$username."', '".$email."',
-'".$blocktype."', '".$phonenumber."', '".$apartmentnumber."', '".$totaldebt."', '".$submitdate."', '".$feedescription."')";
-      $sql2 = "INSERT INTO totaldebtyear(totaldebt,submitdate) VALUES ('$totaldebt','$submitdate')";
-       if($formValid == true){
-       $dbconnection->query($sql2);
-           if(mysqli_query($dbconnection, $sql1)){
-           echo '<script language="javascript">';
-            echo 'alert("User information added to the database successfully")';
-             echo '</script>';
-           } 
-          } else {
-           echo " User information has not been added to the database." ;
-
-        }
+ $sql = "SELECT id FROM user";
+ $result = mysqli_query($dbconnection, $sql);
+if(mysqli_num_rows($result) > 0){
+while ($row = mysqli_fetch_array($result)) {
+      $sql1 = "INSERT INTO feestransaction (userid,monthlydue,submitdate,feedescription) 
+      VALUES('$row[id]','$monthlydue','$submitdate','$feedescription')";
+       $dbconnection->query($sql1);     
   }
 }
-
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
+}
 }
 
 ?>
+<script type="text/javascript">
+    function clicked() {
+       if (confirm('Do you want to submit?')) {
+           clicked.submit();
+       } else {
+           return false;
+       }
+    }
+
+</script>
 
 
-<div id="did">
-<div id="logg">
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
-  	&nbsp;	&nbsp;<input type="text" name="userid" placeholder="Userid(e.g 1)" value="<?php if ( isset($userid) ) echo $userid; ?>" required> 
 
-   	&nbsp;	&nbsp;<input type="text" name="username" placeholder="Username(e.g user bir)" value="<?php if ( isset($username) ) echo $username; ?>" required > 
-         <span class="error">* <br>  <?php echo $nameErr;?></span> 
-
-<br> 
-	&nbsp;	&nbsp;<input type="text" name="email" placeholder="E-mail (e.g example@gmail.com)" value="<?php if ( isset($email) ) echo $email; ?>" required> 
-      <span class="error">* <br> <?php echo  $emailErr;?></span> 
-
+<div class="logg">
+<center>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" style="width: 550px;" >               
+     <input type="text" name="monthlydue" placeholder="Monthly amount" required>
+      <br><input type="text" name="feedescription" placeholder="Fee description" required>
   <br>
 
-&nbsp;	&nbsp; <input type="text" name="blocktype" placeholder="Block Type (e.g A)" value="<?php if ( isset($blocktype) ) echo $blocktype; ?>" required>
-     <span class="error"> * <br> <?php echo  $blcktypeErr;?></span> 
 
-<br>
+            <label for="submitdate"></label>
+             <input type="date" name="submitdate" > <br>
 
- 	&nbsp;	&nbsp;<input type="text" name="phonenumber" placeholder="Phone Number(e.g +912345678901)" value="<?php if ( isset($phonenumber) ) echo $phonenumber; ?>" required>
-          <span class="error"> * <br> <?php echo $phonenumErr;?></span> 
+  <input type="submit" onclick="return confirm('Are you sure you want to add a new payment?')" name="submit" value="Submit"><br>
 
-     <br>
-  	&nbsp;	&nbsp;<input type="text" name="apartmentnumber" placeholder="Apartment Number" value="<?php if ( isset($apartmentnumber) ) echo $apartmentnumber; ?>" required>
-             <span class="error"> * <br> <?php echo  $aptnumbErr;?></span> 
-
-     <br>
-             
-      &nbsp;&nbsp; <input type="text" name="totaldebt" placeholder="Total debt (e.g 1)" value="<?php if ( isset($totaldebt) ) echo $totaldebt; ?>" required>
-      <br>
-      &nbsp;&nbsp; <input type="text" name="feedescription" placeholder="feedescription (e.g dues)" value="<?php if ( isset($feedescription) ) echo $feedescription; ?>" required>
-  <br>
-            <label for="submitdate">Submit Date</label>
-             <input type="date" name="submitdate" value="<?php if ( isset($submitdate) ) echo $submitdate; ?>"> <br>
-
-  	&nbsp;	&nbsp;<input type="submit" name="submit" value="Submit"><br>
-
-	&nbsp;	&nbsp;  <a href="HTMLPage2.php.php"> <input id="button" type="button" value="Go to Admin Dashboard" ></a>
+	 <a href="HTMLPage2.php.php"> <input id="button" type="button" value="Go to Admin Dashboard" ></a>
 </form>
+</center>
+</div>
+<br><br>
 
-</div>
-</div>
 <?php
-include "config/footer.php";
+include "../config/footer.php";
 ?>
 </body>
 </html>

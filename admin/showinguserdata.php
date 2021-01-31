b27d08ef467adb59ba85b0a45c36fc2e6d016ec6
RavanSA <58227337+RavanSA@users.noreply.github.com>
2021-01-31 17:16:16 +0400

    Update showinguserdata.php
    creation of old users

--- admin/showinguserdata.php
+++ admin/showinguserdata.php
@@ -1,4 +1,4 @@
-﻿
+
 <?php
  session_start();
 ?>
@@ -30,6 +30,17 @@ if(!isset($_SERVER['HTTP_REFERER'])){
         table tr td:last-child a{
             margin-right: 15px;
         }
+        #footer {
+   position:fixed;
+   left:0px;
+   bottom:0px;
+   height:30px;
+   width:100%;
+}
+* html #footer {
+   position:absolute;
+   top:expression((0-(footer.offsetHeight)+(document.documentElement.clientHeight ? document.documentElement.clientHeight : document.body.clientHeight)+(ignoreMe = document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop))+'px');
+}
        
     </style>
 </head>
@@ -37,6 +48,12 @@ if(!isset($_SERVER['HTTP_REFERER'])){
 
 <?php
 include "../config/header.php";
+                      
+                        $host = "localhost";
+                        $user = "root";
+                        $pass = "";
+                        $db = "mydb";
+                        $dbconnection = new mysqli($host, $user, $pass,$db);
 ?>
 <br> <br>
  <center><h1><i class="far fa-building" style="font-size:40px"> 
@@ -48,11 +65,72 @@ include "../config/header.php";
   <div class="alert alert-info alert-dismissible" style="width: 93%;">
     <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
     <strong>Read before taking any action!</strong> This section is used for track user information.
-    If the "Moved Date" column shows "0000-00-00", it means the resident <strong> not moved </strong>.
-    Otherwise, the resident moved out of the apartment.
+    If the "Moved Date" column shows "0000-00-00" or empty, it means the resident <strong> not moved </strong>.
+    Otherwise, the resident moved out of the apartment. To see old users <a data-toggle="modal" href="#myModal"> click here</a>
   </div>
 </div>
 </center>
+<div class="container">
+<div class="modal fade bd-example-modal-lg" id ="myModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content" style="width: 700px;">
+        <div class="modal-header">
+          <h4 class="modal-title" style="padding-right:200px">Old Users</h4>
+        </div>
+        <div class="modal-body">
+          <p>
+                      <?php
+
+
+$sql1 = "SELECT * FROM user WHERE moveddate IS NOT NULL OR moveddate <> '0000-00-00'";  
+$resultt = mysqli_query($dbconnection, $sql1);
+           echo "";
+       echo "<table border=\"1\" bordercolor=\"grey\" style=\"background-color:white;width:30%;\"  cellpadding=\"2\" cellspacing=\"1\">";
+    echo"<tr>";
+                                            echo "<th>Username</th>";
+                                        echo "<th>Email</th>";
+                                        echo "<th>Phone Number</th>";
+                                        echo "<th>Block Type</th>";
+                                        echo "<th>Apartment Number</th>";
+                                        echo "<th>Start Date</th>";
+                                        echo "<th>End Date</th>";
+                                        echo "<th>Delete</th>";
+
+    echo"</tr>";
+     while ($roww = mysqli_fetch_array($resultt)) { 
+echo"<tr>"; 
+          echo "<td>". $roww['username'] ."</td>";
+          echo "<td>". $roww['email'] ."</td>";
+          echo "<td>". $roww['phonenumber'] ."</td>";
+          echo "<td>". $roww['blocktype'] ."</td>";
+          echo "<td>". $roww['apartmentnumber'] ."</td>";
+          echo "<td>". $roww['dstart'] ."</td>";
+          echo "<td>". $roww['moveddate'] ."</td>";
+                                                 echo "<td>";
+                                            echo "<a href='deleteuserdata.php?id=". $roww['id'] ."' title='Delete Record' data-toggle='tooltip'><span class='fas fa-user-times fas-2x'></span></a>";
+                                        echo "</td>";
+            
+        echo"</tr>";
+       }
+    echo"</table>";
+
+          ?>
+          </p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<script>
+$(document).ready(function(){
+  $("#myBtn").click(function(){
+    $("#myModal").modal();
+  });
+});
+</script>
 <center>
 
     <div class="wrapper">
@@ -62,14 +140,9 @@ include "../config/header.php";
                     <div class="page-header clearfix"> 
                     </div>
                     <?php
-                      
-                        $host = "localhost";
-                        $user = "root";
-                        $pass = "";
-                        $db = "mydb";
-                        $dbconnection = new mysqli($host, $user, $pass,$db);
 
-                    $sql = "SELECT * FROM user";
+
+                    $sql = "SELECT * FROM user WHERE moveddate IS NULL OR moveddate='0000-00-00' ";
                     if($result = mysqli_query($dbconnection, $sql)){
                         if(mysqli_num_rows($result) > 0){
                             echo "<table class='table table-bordered table-striped'>";
@@ -83,7 +156,6 @@ include "../config/header.php";
                                         echo "<th>Apartment Number</th>";
                                         echo "<th>Moved in</th>";
                                         echo "<th>If moved</th>";
-                                        echo "<th>Moved Date</th>";
                                         echo "<th>Update, Delete</th>";
                                        
 
@@ -100,11 +172,9 @@ include "../config/header.php";
                                         echo "<td>" . $row['dstart'] . "</td>";
 
                                         echo "<td>" . $row['ifmoved'] . "</td>";
-                                        echo "<td>" . $row['moveddate'] . "</td>";
 
                                        echo "<td>";
                                          echo "<a href='updateuserdata.php?id=". $row['id'] ."' title='Update Record' data-toggle='tooltip'><span class='fas fa-pencil-alt fas-2x'></span></a>";
-                                            echo "<a href='deleteuserdata.php?id=". $row['id'] ."' title='Delete Record' data-toggle='tooltip'><span class='fas fa-user-times fas-2x'></span></a>";
                                         echo "</td>";
                                         
                                     echo "</tr>";
@@ -116,7 +186,7 @@ include "../config/header.php";
                             echo "<p><em>No records were found.</em></p>";
                         }
                     } else{
-                        echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+                        echo "ERROR: Could not able to execute  " ;
                     }
                     mysqli_close($dbconnection);
                     ?>
@@ -125,8 +195,13 @@ include "../config/header.php";
         </div>
     </div>
 </center>
+
+<br><br><br>
+<footer id="footer">
 <?php
 include "../config/footer.php";
 ?>
+</footer>
+
 </body>
 </html>
